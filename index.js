37210515ae925d5d506d23e488a9218f3b942aad
Daniel Biondi <biondidaniel94@gmail.com>
2024-08-26 19:08:49 -0300

    biondi

--- index.js
+++ index.js
@@ -38,7 +38,7 @@ app.post('/submit', (req, res) => {
   const mailOptions = {
     from: process.env.EMAIL_USER,
     to: process.env.EMAIL_USER, // Enviar o e-mail para o mesmo endereço
-    subject: `Nova Solicitação de TI: ${assunto}`,
+    subject: `Suporte TI: ${assunto}`,
     html: `
       <html>
       <head>
@@ -47,117 +47,82 @@ app.post('/submit', (req, res) => {
             font-family: 'Roboto', sans-serif;
             margin: 0;
             padding: 0;
-            background: #041b33;
-            color: #ffffff;
+            background: #ffffff; /* Fundo branco */
+            color: #333;
             text-align: center;
           }
           .container {
             max-width: 600px;
             margin: 20px auto;
             background: #ffffff;
-            color: #041b33;
-            border-radius: 24px;
+            color: #333;
+            border-radius: 12px;
             padding: 20px;
-            box-shadow: 0 12px 36px rgba(0, 0, 0, 0.2);
-            border: 1px solid #041b33;
+            box-shadow: 0 8px 24px rgba(0, 0, 0, 0.1);
+            border: 1px solid #ddd;
             position: relative;
             overflow: hidden;
-            animation: fadeIn 1s ease-in-out;
           }
-          @keyframes fadeIn {
-            from { opacity: 0; transform: translateY(20px); }
-            to { opacity: 1; transform: translateY(0); }
+          .header {
+            background: #ffffff;
+            padding: 10px 0;
           }
-          .container:before {
-            content: '';
-            position: absolute;
-            top: -50%;
-            left: -50%;
-            width: 200%;
-            height: 200%;
-            background: radial-gradient(circle at top left, rgba(4, 27, 51, 0.6), rgba(205, 209, 211, 0.6));
-            background-blend-mode: overlay;
-            animation: gradientShift 15s ease infinite;
-            z-index: -1;
-          }
-          @keyframes gradientShift {
-            0% { background-position: 0% 0%; }
-            50% { background-position: 100% 100%; }
-            100% { background-position: 0% 0%; }
+          .header img {
+            max-width: 100%;
+            height: auto;
+            border-radius: 12px 12px 0 0;
+            border-bottom: 4px solid #041b33; /* Linha inferior azul para destaque */
           }
           h1 {
             font-weight: 700;
             font-size: 2.5rem;
-            margin-bottom: 1rem;
+            margin: 1rem 0;
             color: #041b33;
             font-family: 'Poppins', sans-serif;
             letter-spacing: 1px;
             text-shadow: 1px 1px 3px rgba(0, 0, 0, 0.2);
-            background: linear-gradient(135deg, #041b33, #003366);
+            background: linear-gradient(135deg, #041b33, #041b33);
             -webkit-background-clip: text;
             -webkit-text-fill-color: transparent;
-            animation: slideIn 1s ease-out;
-          }
-          @keyframes slideIn {
-            from { transform: translateX(-50px); opacity: 0; }
-            to { transform: translateX(0); opacity: 1; }
+            margin-bottom: 1.5rem;
           }
           p {
-            font-size: 1.6rem;
+            font-size: 1.2rem;
             margin: 0.5rem 0;
+            line-height: 1.5;
           }
           .footer {
             margin-top: 20px;
             font-size: 1rem;
             color: #041b33;
             border-top: 2px solid #041b33;
-            padding-top: 10px;
+            padding: 10px 0;
             position: relative;
-            background: linear-gradient(135deg, rgba(4, 27, 51, 0.1), rgba(205, 209, 211, 0.1));
-            overflow: hidden;
-          }
-          .footer:before {
-            content: '';
-            position: absolute;
-            top: -50%;
-            left: -50%;
-            width: 200%;
-            height: 200%;
-            background: radial-gradient(circle at bottom right, rgba(4, 27, 51, 0.2), rgba(205, 209, 211, 0.2));
-            animation: footerWave 20s linear infinite;
-            z-index: -1;
-          }
-          @keyframes footerWave {
-            0% { background-position: 0% 0%; }
-            50% { background-position: 100% 100%; }
-            100% { background-position: 0% 0%; }
+            background: #ffffff;
           }
           .button {
             display: inline-block;
             padding: 10px 20px;
             border-radius: 30px;
-            background: linear-gradient(135deg, #041b33, #003366);
+            background: #041b33;
             color: #ffffff;
             text-decoration: none;
             font-weight: bold;
             font-size: 1.2rem;
-            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.1);
             transition: background 0.3s ease, transform 0.3s ease;
-            animation: pulse 2s infinite;
-          }
-          @keyframes pulse {
-            0% { transform: scale(1); }
-            50% { transform: scale(1.05); }
-            100% { transform: scale(1); }
           }
           .button:hover {
-            background: linear-gradient(135deg, #002d50, #001f3f);
+            background: #003366;
             transform: translateY(-2px);
           }
         </style>
       </head>
       <body>
         <div class="container">
+          <div class="header">
+            <img src="https://app.tangerino.com.br/Tangerino/pages/image?banner=14727" alt="Banner">
+          </div>
           <h1>Nova Solicitação de TI</h1>
           <p><strong>Nome:</strong> ${nome}</p>
           <p><strong>Email:</strong> ${email}</p>
@@ -190,5 +155,3 @@ app.post('/submit', (req, res) => {
 app.listen(port, () => {
   console.log(`Servidor rodando na porta ${port}`);
 });
-
-
