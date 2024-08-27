28325026dafb074cc6aa6c8288f64ae0ef21c580
Sakuma <yudisakuma@gmail.com>
2024-08-27 01:08:58 -0300

    changed redis url

--- src/redis.js
+++ src/redis.js
@@ -1,10 +1,10 @@
 const Redis = require('ioredis');
-
+require('dotenv').config()
 const redisOptions = {
   host: 'localhost',
   port: 6379,
 };
 
-const redisClient = new Redis(redisOptions);
+const redisClient = new Redis(process.env.REDIS_CLIENT);
 
 module.exports = redisClient;
